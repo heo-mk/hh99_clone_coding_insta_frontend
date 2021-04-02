80aaef18e126b0a0a8ab1e9470799fd011ed6e40
Daeho Lee <67696504+leedaeho1188@users.noreply.github.com>
2021-04-02 23:13:37 +0900

    업데이트

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # hh99_clone_coding
-새벽님 땡큐
---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+
+
+const Header = () => {
+  return(
+    <React.Fragment>
+      <div>
+        
+      </div>
+    </React.Fragment>
+  )
+
+}
+
+export default Header
